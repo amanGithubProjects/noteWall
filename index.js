671fddb3b3f70142ce142e6f5d81ce124d79e2f2
amanGithubProjects <80571575+amanGithubProjects@users.noreply.github.com>
2023-09-07 15:47:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const inputBox = document.getElementById('input_box');
+const listConatainer = document.getElementById('list_container');
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("Please write something!!!")
+    }else{
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        listConatainer.appendChild(li);
+
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData()
+}
+
+listConatainer.addEventListener('click', function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+        saveData()
+    }else if(e.target.tagName==='SPAN'){
+        e.target.parentElement.remove();
+        saveData()
+    }
+});
+
+function saveData(){
+    localStorage.setItem("data", listConatainer.innerHTML)
+};
+
+function showTask(){
+    listConatainer.innerHTML = localStorage.getItem("data");
+};
+
+showTask();
